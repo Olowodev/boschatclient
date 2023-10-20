c353dde3dd37dfef661e76423da7a923f01230e1
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2023-10-20 16:01:14 +0100

    commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,85 @@
+"use client"
+
 import Image from 'next/image'
+import { useEffect, useRef, useState } from 'react'
 import styles from './page.module.css'
 
 export default function Home() {
+  const user1 = useRef(null)
+  const user2 = useRef(null)
+  const [servers, setServers] = useState({
+    iceservers: [
+      {
+        urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+      }
+    ]
+  })
+  // const [stream, setStream] = useState()
+  // const [remoteStream, setRemoteStream] = useState(new MediaStream())
+  const [peerConnection, setPeerConnection] = useState(new RTCPeerConnection(servers))
+  const [retry, setRetry] = useState(0)
+  
+
+  const setUp = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
+      // setStream(streamm)
+      user1.current.srcObject = stream
+      createOffer(stream)
+    } catch (err) {
+      console.log(err)
+      // setUp()
+      setRetry((retry) => retry + 1)
+    }
+  }
+
+  // console.log(retry)
+
+  const createOffer = async (stream) => {
+    // setPeerConnection(new RTCPeerConnection())
+    const remoteStream = new MediaStream()
+    // setRemoteStream(new MediaStream())
+    user2.current.srcObject = remoteStream
+
+    if (stream) {
+      stream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, stream)
+      })
+    }
+
+    peerConnection.ontrack = (event) => {
+      event.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack()
+      })
+    }
+
+    peerConnection.onicecandidate = async (event) => {
+      if(event.candidate) {
+        console.log('New ICE candidate:', event.candidate)
+      }
+    }
+
+    const offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer)
+  }
+
+  // useEffect(() => {
+  //   createOffer()
+  // }, [stream])
+
+  //listen for permission change
+  //display which webcam and microphone its connected to
+  
+  useEffect(() => {
+    setUp()
+    console.log('checking')
+  }, [])
+
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <video ref={user1} className='video-player' autoPlay playsInline id='user-1'></video>
+      <video ref={user2} className='video-player' autoPlay playsInline id='user-2'></video>
     </main>
   )
 }
