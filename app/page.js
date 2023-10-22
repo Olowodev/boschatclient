008e3a85cbd7e456fb43bab1383984fdc0e059c1
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2023-10-22 16:22:37 +0100

    test new way

--- app/page.js
+++ app/page.js
@@ -11,156 +11,292 @@ let peerConnection;
   let remoteStream;
   let stream;
 export default function Home() {
-  const user1 = useRef(null)
-  const user2 = useRef(null)
-  const [servers, setServers] = useState({
-    iceServers: [
-      {
-        urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
-      }
-    ]
-  })
-  // const [stream, setStream] = useState()
-  // const [remoteStream, setRemoteStream] = useState(new MediaStream())
-  // const [peerConnection, setPeerConnection] = useState(new RTCPeerConnection(servers))
-  const [retry, setRetry] = useState(0)
+  // const user1 = useRef(null)
+  // const user2 = useRef(null)
+  // const [servers, setServers] = useState({
+  //   iceServers: [
+  //     {
+  //       urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+  //     }
+  //   ]
+  // })
+  // // const [stream, setStream] = useState()
+  // // const [remoteStream, setRemoteStream] = useState(new MediaStream())
+  // // const [peerConnection, setPeerConnection] = useState(new RTCPeerConnection(servers))
+  // const [retry, setRetry] = useState(0)
 
 
-  const setUp = async () => {
-    // try {
-      stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-      // setStream(streamm)
-      user1.current.srcObject = stream
-      // createOffer()
-    // } catch (err) {
-    //   console.log(err)
-    //   // setUp()
-    //   setRetry((retry) => retry + 1)
-    // }
-  }
+  // const setUp = async () => {
+  //   // try {
+  //     stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+  //     // setStream(streamm)
+  //     user1.current.srcObject = stream
+  //     // createOffer()
+  //   // } catch (err) {
+  //   //   console.log(err)
+  //   //   // setUp()
+  //   //   setRetry((retry) => retry + 1)
+  //   // }
+  // }
 
-  // console.log(retry)
+  // // console.log(retry)
 
   
 
-  const createPeerConnection = async () => {
-    // setPeerConnection(new RTCPeerConnection())
-    remoteStream = new MediaStream()
-    peerConnection = new RTCPeerConnection(servers)
-    // setRemoteStream(new MediaStream())
-    user2.current.srcObject = remoteStream
+  // const createPeerConnection = async () => {
+  //   // setPeerConnection(new RTCPeerConnection())
+  //   remoteStream = new MediaStream()
+  //   peerConnection = new RTCPeerConnection(servers)
+  //   // setRemoteStream(new MediaStream())
+  //   user2.current.srcObject = remoteStream
 
-    // if(!stream) {
-    //   stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
-    //   user1.current.srcObject = stream
-    // }
+  //   // if(!stream) {
+  //   //   stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
+  //   //   user1.current.srcObject = stream
+  //   // }
 
     
-      stream.getTracks().forEach((track) => {
-        peerConnection.addTrack(track, stream)
-      })
+  //     stream.getTracks().forEach((track) => {
+  //       peerConnection.addTrack(track, stream)
+  //     })
 
-    peerConnection.ontrack = (event) => {
-      console.log(event)
-      event.streams[0].getTracks().forEach((track) => {
-        remoteStream.addTrack(track)
-      })
-    }
+  //   peerConnection.ontrack = (event) => {
+  //     console.log(event)
+  //     event.streams[0].getTracks().forEach((track) => {
+  //       remoteStream.addTrack(track)
+  //     })
+  //   }
 
-    peerConnection.onicecandidate = async (event) => {
-      if (event.candidate) {
-        console.log('New ICE candidate:', event.candidate)
-        socket.emit("messageFromPeer", {type: 'candidate', candidate: event.candidate})
-      }
-    }
+  //   peerConnection.onicecandidate = async (event) => {
+  //     if (event.candidate) {
+  //       console.log('New ICE candidate:', event.candidate)
+  //       socket.emit("messageFromPeer", {type: 'candidate', candidate: event.candidate})
+  //     }
+  //   }
 
-  }
+  // }
 
-  const createOffer = async () => {
-    console.log("creating offer")
-    await createPeerConnection()
-    const offer = await peerConnection.createOffer()
-    await peerConnection.setLocalDescription(offer)
+  // const createOffer = async () => {
+  //   console.log("creating offer")
+  //   await createPeerConnection()
+  //   const offer = await peerConnection.createOffer()
+  //   await peerConnection.setLocalDescription(offer)
 
-    console.log("Offer:", offer)
+  //   console.log("Offer:", offer)
 
-    socket.emit("messageFromPeer", {type: "offer", offer: offer})
-  }
+  //   socket.emit("messageFromPeer", {type: "offer", offer: offer})
+  // }
 
-  let createAnswer = async (offer) => {
-    console.log("creating answer")
-    await createPeerConnection()
-    await peerConnection.setRemoteDescription(offer)
+  // let createAnswer = async (offer) => {
+  //   console.log("creating answer")
+  //   await createPeerConnection()
+  //   await peerConnection.setRemoteDescription(offer)
 
-    let answer = await peerConnection.createAnswer()
-    await peerConnection.setLocalDescription(answer)
+  //   let answer = await peerConnection.createAnswer()
+  //   await peerConnection.setLocalDescription(answer)
 
-    socket.emit("messageFromPeer", {type: 'answer', answer: answer})
-  }
+  //   socket.emit("messageFromPeer", {type: 'answer', answer: answer})
+  // }
 
-  let addAnswer = async (answer) => {
-    if(!peerConnection.currentRemoteDescription) {
-      peerConnection.setRemoteDescription(answer)
-    }
-  }
-  // useEffect(() => {
+  // let addAnswer = async (answer) => {
+  //   if(!peerConnection.currentRemoteDescription) {
+  //     peerConnection.setRemoteDescription(answer)
+  //   }
+  // }
+  // // useEffect(() => {
+  // //   createOffer()
+  // // }, [stream])
+
+  // //listen for permission change
+  // //display which webcam and microphone its connected to
+
+  // let handleUserJoined = async () => {
+  //   console.log("new user joined the chat")
   //   createOffer()
-  // }, [stream])
+  // }
 
-  //listen for permission change
-  //display which webcam and microphone its connected to
+  // let handleMessageFromPeer = async (message) => {
+  //   console.log("peer sent message")
+  //   if (message.type === "offer") {
+  //     createAnswer(message.offer)
+  //   }
 
-  let handleUserJoined = async () => {
-    console.log("new user joined the chat")
-    createOffer()
+  //   if (message.type === "answer") {
+  //     addAnswer(message.answer)
+  //   }
+
+  //   if (message.type === "candidate") {
+  //     if(peerConnection){
+  //       peerConnection.addIceCandidate(message.candidate)
+  //     }
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   console.log('checking')
+  //   socket.on("connect", () => {
+  //     console.log(socket.id)
+  //   })
+
+  //   socket.on("disconnect", () => {
+  //     console.log(socket.id + "disconnected")
+  //   })
+
+  //   socket.emit("room:create", "123456")
+
+  //   socket.emit("room:join", "123456")
+
+  //   socket.on("user:join", handleUserJoined)
+
+  //   socket.on("messageFromPeer", handleMessageFromPeer)
+
+  //   socket.on("room:join:error", (message) => {
+  //     console.log(message)
+  //   })
+
+  //   // socket.on("room:join", () => {
+  //   //   console.log("testing client to client")
+  //   // })
+
+  //   socket.on("broadcast:received", handleMessageFromPeer)
+  //   setUp()
+  // }, [])
+
+  const user1 = useRef()
+  const user2 = useRef()
+  const rtcConnectionRef = useRef(null)
+  // const socketRef = useRef()
+  const userStreamRef = useRef()
+  const hostRef = useRef(false)
+
+  const handleRoomCreated = async () => {
+    console.log("room created")
+    hostRef.current = true
+    try {
+    const stream = await navigator.mediaDevices.getUserMedia({video: true})
+    userStreamRef.current = stream
+    user1.current.srcObject = stream
+    } catch(err) {
+      console.log(err)
+    }
   }
 
-  let handleMessageFromPeer = async (message) => {
-    console.log("peer sent message")
-    if (message.type === "offer") {
-      createAnswer(message.offer)
+  const handleRoomJoined = async () => {
+    try {
+    const stream = await navigator.mediaDevices.getUserMedia({video:true})
+    userStreamRef.current = stream
+    user1.current.srcObject = stream
+    socket.emit('ready', "1234")
+    } catch(err) {
+      console.log('error', err)
     }
+  }
 
-    if (message.type === "answer") {
-      addAnswer(message.answer)
+  const initiateCall = async () => {
+    try {
+    if(hostRef.current) {
+      rtcConnectionRef.current = createPeerConnection()
+      console.log(userStreamRef.current.getTracks())
+      rtcConnectionRef.current.addTrack(
+        userStreamRef.current.getTracks()[0],
+        userStreamRef.current
+      )
+      // rtcConnectionRef.current.addTrack(
+      //   userStreamRef.current.getTracks()[1],
+      //   userStreamRef.current
+      // )
+      const offer = await rtcConnectionRef.current.createOffer()
+      rtcConnectionRef.current.setLocalDescription(offer)
+      socket.emit('offer', offer, "1234")
     }
+  } catch(err) {
+    console.log(err)
+  }
+  }
 
-    if (message.type === "candidate") {
-      if(peerConnection){
-        peerConnection.addIceCandidate(message.candidate)
+  const ICE_SERVERS = {
+    iceServers: [
+      {
+        urls: 'stun:openrelay.metered.ca:80'
       }
+    ]
+  }
+
+  const createPeerConnection = () => {
+    const connection = new RTCPeerConnection(ICE_SERVERS)
+
+    connection.onicecandidate = handleICECandidateEvent
+
+    connection.ontrack = handleTrackEvent
+    return  connection
+  }
+
+  const handleReceivedOffer = async (offer) => {
+    try {
+    if (!hostRef.current) {
+      rtcConnectionRef.current = createPeerConnection()
+      rtcConnectionRef.current.addTrack(
+        userStreamRef.current.getTracks()[0],
+        userStreamRef.current
+      )
+      // rtcConnectionRef.current.addTrack(
+      //   userStreamRef.current.getTracks()[1],
+      //   userStreamRef.current
+      // )
+      rtcConnectionRef.current.setRemoteDescription(offer)
+
+      const answer = await rtcConnectionRef.current.createAnswer()
+      rtcConnectionRef.current.setLocalDescription(answer)
+      socket.emit('answer', answer, "1234")
     }
+  } catch (err) {
+    console.log(err)
+  }
+  }
+
+  const handleAnswer = (answer) => {
+    rtcConnectionRef.current.setRemoteDescription(answer)
+      .catch((err) => console.log(err))
+  }
+
+  const handleICECandidateEvent = (event) => {
+    if(event.candidate){
+      socket.emit('ice-candidate', event.candidate, "1234")
+    }
+  }
+
+  const handleNewIceCandidateMsg = (incoming) => {
+    const candidate = new RTCIceCandidate(incoming)
+    rtcConnectionRef.current.addIceCandidate(candidate)
+      .catch((e) => console.log(e))
+  }
+
+  const handleTrackEvent = (event) => {
+    user2.current.srcObject = event.streams[0]
   }
 
   useEffect(() => {
-    console.log('checking')
-    socket.on("connect", () => {
-      console.log(socket.id)
+    // socket = socket
+
+    socket.on('connect', ()=>{
+      console.log("it connected")
     })
-
-    socket.on("disconnect", () => {
-      console.log(socket.id + "disconnected")
+    socket.emit('join', "1234")
+    socket.on('created', handleRoomCreated)
+    socket.on('joined', handleRoomJoined)
+    socket.on('ready', initiateCall)
+    // socket.on('leave', onPeerLeave)
+    socket.on('full', () => {
+      console.log('room is full')
     })
+    socket.on('offer', handleReceivedOffer)
+    socket.on('answer', handleAnswer)
+    socket.on('ice-candidate', handleNewIceCandidateMsg)
 
-    socket.emit("room:create", "123456")
-
-    socket.emit("room:join", "123456")
-
-    socket.on("user:join", handleUserJoined)
-
-    socket.on("messageFromPeer", handleMessageFromPeer)
-
-    socket.on("room:join:error", (message) => {
-      console.log(message)
-    })
-
-    // socket.on("room:join", () => {
-    //   console.log("testing client to client")
-    // })
-
-    socket.on("broadcast:received", handleMessageFromPeer)
-    setUp()
+    // return () => socket.disconnect()
   }, [])
+
+
 
 
   return (
